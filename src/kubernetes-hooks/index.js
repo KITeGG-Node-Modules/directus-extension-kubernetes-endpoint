e47142f779ffea1c26edf5980f98e0e63dfd0e30
Anton <antonymousfeedback@gmail.com>
2025-11-07 19:10:28 +0100

    Update action hooks

--- src/kubernetes-hooks/index.js
+++ src/kubernetes-hooks/index.js
@@ -1,67 +1,89 @@
-import { createOrReplaceDeployment } from '../lib/operations/create-update-deployment.js'
-import { parseErrorResponse, updateStatus } from '../lib/util.js'
+import { createOrReplaceDeployment } from '../lib/operations/create-replace-deployment.js'
+import { forwardToKubernetes } from '../lib/util.js'
 import { removeDeployment } from '../lib/operations/remove-deployment.js'
+import { createOrReplaceService } from '../lib/operations/create-replace-service.js'
+import { removeService } from '../lib/operations/remove-service.js'
+import { createOrReplaceVolumeClaim } from '../lib/operations/create-replace-volume-claim.js'
+import { removeVolumeClaim } from '../lib/operations/remove-volume-claim.js'
+import { createOrReplaceSecret } from '../lib/operations/create-replace-secret.js'
+import { removeSecret } from '../lib/operations/remove-secret.js'
+import { createOrReplaceConfigMap } from '../lib/operations/create-replace-config-map.js'
+import { removeConfigMap } from '../lib/operations/remove-config-map.js'
 
-export async function forwardToKubernetes(
-  services,
-  meta,
-  context,
-  handlerFunction
-) {
-  const { key, keys, collection } = meta
-  let ids
-  if (key && !keys) {
-    ids = [key]
-  } else {
-    ids = keys
-  }
-  for (const id of ids) {
-    let _status, _errors, object
-    try {
-      const { ItemsService } = services
-      const service = new ItemsService(collection, {
-        schema: context.schema,
-        accountability: context.accountability,
-      })
-      object = await service.readOne(id)
-    } catch {}
-    try {
-      _status = await handlerFunction(object || id)
-      _errors = null
-    } catch (err) {
-      _status = null
-      _errors = parseErrorResponse(err)
-    }
-    await updateStatus(services, context, collection, id, _status, _errors)
-  }
-}
+function genericAction(args, key, k8sProps, createFunc, removeFunc) {
+  const [{ action }, { services }] = args
 
-export default ({ action }, { services }) => {
-  action('k8s_deployments.items.create', async (meta, context) => {
-    await forwardToKubernetes(
-      services,
-      meta,
-      context,
-      createOrReplaceDeployment
-    )
+  action(`${key}.create`, async (meta, context) => {
+    await forwardToKubernetes(services, meta, context, createFunc)
   })
 
-  action('k8s_deployments.items.update', async (meta, context) => {
-    const k8sProps = ['containers', 'podName']
+  action(`${key}.update`, async (meta, context) => {
     const needsDeploy = Object.keys(meta.payload).reduce(
       (result, key) => result || k8sProps.includes(key),
       false
     )
     if (needsDeploy)
-      await forwardToKubernetes(
-        services,
-        meta,
-        context,
-        createOrReplaceDeployment
-      )
+      await forwardToKubernetes(services, meta, context, createFunc)
   })
 
-  action('k8s_deployments.items.delete', async (meta, context) => {
-    await forwardToKubernetes(services, meta, context, removeDeployment)
+  action(`${key}.delete`, async (meta, context) => {
+    await forwardToKubernetes(services, meta, context, removeFunc)
   })
+}
+
+export default (...args) => {
+  //
+  // Deployments
+
+  genericAction(
+    args,
+    'k8s_deployments.items',
+    ['containers', 'podName'],
+    createOrReplaceDeployment,
+    removeDeployment
+  )
+
+  //
+  // Services
+
+  genericAction(
+    args,
+    'k8s_services.items',
+    ['ports'],
+    createOrReplaceService,
+    removeService
+  )
+
+  //
+  // VolumeClaims
+
+  genericAction(
+    args,
+    'k8s_volumes.items',
+    ['size', 'mountType'],
+    createOrReplaceVolumeClaim,
+    removeVolumeClaim
+  )
+
+  //
+  // Secrets
+
+  genericAction(
+    args,
+    'k8s_secrets.items',
+    ['data'],
+    createOrReplaceSecret,
+    removeSecret
+  )
+
+  //
+  // ConfigMaps
+
+  genericAction(
+    args,
+    'k8s_configs.items',
+    ['data'],
+    createOrReplaceConfigMap,
+    removeConfigMap
+  )
 }
