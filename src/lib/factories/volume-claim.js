ce2c40d6eee5fe0882f198398ad00ef932f6f806
Anton <antonymousfeedback@gmail.com>
2025-11-17 10:51:17 +0100

    Remove unused code

--- src/lib/factories/volume-claim.js
+++ src/lib/factories/volume-claim.js
@@ -1,7 +1,5 @@
 import k8s from '@kubernetes/client-node'
 import { genericMetadata } from '../util/k8s.js'
-import { getKubernetesClient } from 'kitegg-directus-extension-common'
-import { LABEL_NAMESPACE } from '../variables.js'
 
 export function makeVolumeClaim(payload, userId) {
   const volumeClaim = new k8s.V1PersistentVolumeClaim()
@@ -14,29 +12,4 @@ export function makeVolumeClaim(payload, userId) {
     storage: payload.size,
   }
   return volumeClaim
-}
-
-export async function removeVolumeClaim(id) {
-  const client = getKubernetesClient(undefined, k8s.CoreV1Api)
-  const { body: existing } =
-    await client.listPersistentVolumeClaimForAllNamespaces(
-      undefined,
-      undefined,
-      undefined,
-      `${LABEL_NAMESPACE}/objectId=${id}`
-    )
-  if (existing.items.length > 0) {
-    for (const item of existing.items) {
-      await client.deleteNamespacedPersistentVolumeClaim(
-        item.metadata.name,
-        item.metadata.namespace,
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        'Background'
-      )
-    }
-  }
-  return { deleted: id }
 }
